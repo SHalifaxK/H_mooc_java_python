e193b773c35df2d05bf14fc292c22a4add6aef6b
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-03-26 18:32:03 +0300

    Create harj_7.java

---
+++ harj_7.java
@@ -0,0 +1,28 @@
+
+public class Kertolasku {
+
+    public static void main(String[] args) {
+
+        int a = 1337;
+        int b = 42;
+/*
+        
+
+Tee edellistä ohjelmaa vastaava ohjelma, joka laskee kahden kokonaislukumuuttujaan sijoitetun arvon kertolaskun.
+
+Esimerkiksi jos muuttujissa on luvut 2 ja 8, ohjelman tulostus on seuraava:
+2 * 8 = 16
+
+Jos taas muuttujissa on luvut 277 ja 111, ohjelman tulostus on seuraava:
+277 * 111 = 30747
+        
+Kokeile myös kuinka suuren kertolaskun ohjelmasi pystyy laskemaan!
+
+        */
+        // Toteuta ohjelmasi tähän -- ota tarvittaessa mallia 
+        // edellisestä tehtävästä. Muista käyttää muuttujia a ja b!
+        int tulos = a * b;
+        System.out.println(a + " * " + b + " = " + tulos);
+    }
+
+}
