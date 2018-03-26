e77c0bf8aeb15231631f026682abdbb70966c43b
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-03-26 14:51:59 +0300

    Create harj_3.java

---
+++ harj_3.java
@@ -0,0 +1,19 @@
+/*Tee ohjelma, jonka tulostus on seuraava:
+*
+*     *
+*    ***
+*   *****
+*  *******
+* *********
+*     *
+*/
+public class Puu {
+    public static void main (String[] args){
+        System.out.println('     *');
+        System.out.println('    ***');
+        System.out.println('   *****');
+        System.out.println('  *******');
+        System.out.println(' *********');
+        System.out.println('     *');
+    }
+}
