f5670b774be73064aaada06dcda69a142137a81a
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-03-26 18:48:08 +0300

    Create harj_9.java

---
+++ harj_9.java
@@ -0,0 +1,32 @@
+
+public class Nelilaskin {
+
+    public static void main(String[] args) {
+        int eka = 5;
+        int toka = 3;
+
+        System.out.println(eka + " + " + toka + " = " + (eka + toka));
+/*        
+Tehtäväpohjassa on ohjelma, jossa on kaksi muuttujaa. 
+Ohjelman tulostus on tällä hetkellä seuraava.
+5 + 3 = 8
+
+Muokkaa ohjelmaa siten, että ohjelma laskee myös 
+lukujen erotuksen, tulon, ja jakolaskun.
+5 + 3 = 8
+5 - 3 = 2
+5 * 3 = 15
+5 / 3 = 1.6666666666666667
+
+Huom! Jos muuttujien arvoja muutetaan, tulee myös tulostuksen muuttua.
+1 + 3 = 4
+1 - 3 = -2
+1 * 3 = 3
+1 / 3 = 0.3333333333333333
+*/
+        // Toteuta loput nelilaskimesta tähän.
+        System.out.println(eka + " - " + toka + " = " + (eka - toka));
+        System.out.println(eka + " * " + toka + " = " + (eka * toka));
+        System.out.println(eka + " / " + toka + " = " + (1.0 * eka / toka));
+    }
+}
