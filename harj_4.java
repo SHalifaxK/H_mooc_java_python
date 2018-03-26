4c7bb1c7cc658399023f3f55f768b4a7b7d044b2
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-03-26 17:40:33 +0300

    Create harj_4.java

---
+++ harj_4.java
@@ -0,0 +1,42 @@
+
+public class MuuttuvatMuuttujat {
+
+    public static void main(String[] args) {
+        // MUUTA NÄITÄ:
+/*
+        Kanoja:
+3
+Pekonia (kg):
+5.5
+
+Tässä vielä tiivistelmä:
+3
+5.5
+
+Muuta ohjelmaa annetuista kohdista niin että tuloste on:
+
+Kanoja:
+9000
+Pekonia (kg):
+0.1
+
+Tässä vielä tiivistelmä:
+9000
+0.1
+
+        */
+        int kanojenLkm = 9000;
+        double pekoninPaino = 0.1;
+
+        // ÄLÄ MUUTA NÄITÄ:
+        System.out.println("Kanoja:");
+        System.out.println(kanojenLkm);
+        System.out.println("Pekonia (kg):");
+        System.out.println(pekoninPaino);
+        System.out.println("");
+        System.out.println("Tässä vielä tiivistelmä:");
+        System.out.println(kanojenLkm);
+        System.out.println(pekoninPaino);
+    }
+
+}
