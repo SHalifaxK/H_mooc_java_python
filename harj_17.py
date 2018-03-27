3a9a865c69e33245618391fd3a1827059a4dc70b
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-03-27 13:08:56 +0300

    Create harj_17.py

---
+++ harj_17.py
@@ -0,0 +1,15 @@
+'''
+Tee ohjelma, joka kysyy käyttäjältä kokonaisluvun 
+ja tulostaa merkkijonon "Orwell" jos luku on täsmälleen 1984.
+
+Anna luku: 1983
+
+Anna luku: 1984
+Orwell
+
+'''
+luku = int(input('Anna luku: '))
+if luku == 1984:
+    print('Orwell')
+else:
+    pass
