efa08d08d8bbc6055c98890696868508e461f0b7
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-03-27 13:06:38 +0300

    Create harj_16.py

---
+++ harj_16.py
@@ -0,0 +1,15 @@
+'''
+Tee ohjelma, joka kysyy käyttäjältä kokonaisluvun 
+ja tulostaa merkkijonon "Ylinopeussakko!" jos luku on suurempi kuin 120.
+
+Kerro nopeus: 15
+
+Kerro nopeus: 135
+Ylinopeussakko!
+
+'''
+nopeus = int(input('Kerro nopeus: '))
+if nopeus > 120:
+    print('Ylinopeussakko')
+else:
+    pass
