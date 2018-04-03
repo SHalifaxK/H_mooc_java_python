9e27e21e91f1ab44812f982d6dd7637168d90e7f
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-04-03 11:45:49 +0300

    Update harj_5.java

--- harj_5.java
+++ harj_5.java
@@ -1,28 +1,23 @@
-public class SekunnitVuodessa {
-
-    public static void main(String[] args) {
-
-        // Tässä on hieman puolitiehen jäänyt versio vuoden sekunnit
-        // laskevasta ohjelmasta. Korjaapa se!
-        /*
-        
-
-Täydennä tehtäväpohjassa olevaa ohjelmaa siten, että se laskee kuinka monta sekuntia on vuodessa. 
-Voit olettaa, että vuodessa on 365 päivää.
+/*
+Tee ohjelma, joka tulostaa kokonaisluvut väliltä 100–1.
 
 Ohjelman tulostus on seuraava:
-Vuodessa on X sekuntia.
-X:n kohdalle tulee ohjelmasi laskema tulos. Huom! Hyödynnä tässä tehtävässä muuttujia.
 
-        */
-        int paiviaVuodessa = 365;
-        int tuntejaPaivassa = 24;
-        int minuuttejaTunnissa = 60;
-        int sekuntejaMinuutissa = 60;
-        int sekuntejaVuodessa = paiviaVuodessa * tuntejaPaivassa * minuuttejaTunnissa * sekuntejaMinuutissa;
+100
+99
+98
+(välissä paljon rivejä)
+3
+2
+1
+*/
+public class SadastaYhteen {
 
-        // Älä muuta alla olevaa tulostuslausetta (muokkaa vain yllä olevia muuttujia ja laskua)
-        System.out.println("Vuodessa on " + sekuntejaVuodessa + " sekuntia.");
+    public static void main(String[] args) {
+        int i = 100;
+        while (i>0){
+            System.out.println(i);
+            i--;
+        }
     }
-
 }
