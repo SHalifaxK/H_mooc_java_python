4fb0e6c6ebc810304b8759200478fdbd6be02510
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-04-03 12:00:40 +0300

    Update harj_8.java

--- harj_8.java
+++ harj_8.java
@@ -1,41 +1,39 @@
 
-public class KolmenLuvunKeskiarvo {
+/*
+Kirjoita ohjelma, joka kysyy käyttäjältä lukua. 
+Jos luku on positiivinen, tulostetaan "Juupas", jonka jälkeen 
+kysytään lukua uudestaan. Jos luku on negatiivinen, tulostetaan "Eipäs" ja 
+kysytään lukua uudestaan. Ohjelman suoritus loppuu kun käyttäjä syöttää luvun 0.
+
+Kun mietit minkälaisilla syötteillä ohjelma toimii toivotusti, erilaisia 
+toimintoja on 3. Ohjelmaa kannattaa siis testata ainakin kolmella erilaisella 
+syötteellä, jonka lisäksi lienee hyvä tarkastaa ohjelman toiminta hyvin 
+pienillä (esim. -10000) ja hyvin suurilla syötteillä (10000).
+
+Alla vielä toivottu toiminnallisuus vuokaaviona.
+st=>start: Alku io=>inputoutput: lue luku cond=>condition: 
+luku > 0 cond2=>condition: luku < 0 io2=>inputoutput: "Juupas" io3=>inputoutput: 
+"Eipäs" e=>end: Loppu st->io->cond cond(yes, right)->io2->io cond(no)->cond2 
+cond2(yes, right)->io3->io cond2(no)->e
+
+*/
+import java.util.Scanner;
+
+public class JuupasEipas {
 
     public static void main(String[] args) {
-/*
-        
-
-Tehtäväpohjassa on ohjelma, jossa on kolme muuttujaa. Ohjelman tulostus on tällä hetkellä seuraava.
-eka: 9
-toka: 7
-kolmas: 6
-
-Muokkaa ohjelmaa siten, että ohjelma tulostaa myös muuttujien keskiarvon.
-eka: 9
-toka: 7
-kolmas: 6
-keskiarvo: 7.333333333333333
-
-Huom! Jos muuttujien arvoja muutetaan, tulee myös tulostuksen muuttua.
-eka: 5
-toka: 7
-kolmas: 4
-keskiarvo: 5.333333333333333
-
-
-        */
-        int eka = 9;
-        int toka = 7;
-        int kolmas = 6;
-        
-        System.out.println("eka: " + eka);
-        System.out.println("toka: " + toka);
-        System.out.println("kolmas: " + kolmas);
-        
-        // Toteuta ohjelmasi tähän. Muista käyttää muuttujia eka, toka ja kolmas!
-        double keskiarvo = (eka+toka+kolmas)/3.0;
-        System.out.println("keskiarvo: "+keskiarvo);
-    
+        Scanner lukija = new Scanner(System.in);
+        // kirjoita ohjelma tänne
+        System.out.print("Anna lukuja, nolla lopettaa: ");
+        while (true){
+            int luku = Integer.parseInt(lukija.nextLine());
+            if (luku>0){
+                System.out.println("Juupas");
+            }else if (luku<0){
+                System.out.println("Eipäs");
+            }else if (luku==0){
+                break;
+            } 
+        }
     }
 }
-
