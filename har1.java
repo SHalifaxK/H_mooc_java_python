dfa4fb5915e2176b0b5fa226dbfd684551625158
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-04-03 11:00:23 +0300

    Update har1.java

--- har1.java
+++ har1.java
@@ -1,7 +1,43 @@
-public class Nimi {
+/*
+Kaverisi on hahmotellut summaamiseen ja ehtolauseen toimintaan 
+liittyvää testiohjelmaa. Testiohjelmassa ohjelman alussa luettuun 
+lukuun lisätään käyttäjältä luettu toinen luku. Luvun lisäämisen tulee 
+tapahtua vain jos toisena luettu luku on positiivinen.
+
+Ohjelma ei tällä hetkellä kuitenkaan toimi ihan toivotusti ja 
+siinä näkyy punaista. Kaverisi ohjelma näyttää seuraavalta:
+
+Korjaa kaverisi ohjelma. Alla esimerkkitulosteita toimivasta ohjelmasta.
+
+32
+7
+Summa: 39
+
+9
+-3
+Summa: 9
+
+
+*/
+import java.util.Scanner;
+
+public class PositiivisenLuvunLisaaminen {
 
     public static void main(String[] args) {
-        System.out.println("Ada Lovelace");
-
+        Scanner lukija = new Scanner(System.in);
+        int luettu1 = Integer.parseInt(lukija.nextLine());
+        int summa = 0;
+        
+        if (luettu1 > 0) {
+            summa += luettu1;
+        }
+        
+        int luettu2 = Integer.parseInt(lukija.nextLine());
+        
+        if (luettu2 > 0) {
+            summa += luettu2;
+        }
+        
+        System.out.println("Summa: " + summa);
     }
 }
