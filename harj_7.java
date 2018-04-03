d3b70ab00d66d35279000dee2f4be8b34d0798ae
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-04-03 11:53:12 +0300

    Update harj_7.java

--- harj_7.java
+++ harj_7.java
@@ -1,28 +1,54 @@
-
-public class Kertolasku {
-
-    public static void main(String[] args) {
-
-        int a = 1337;
-        int b = 42;
 /*
-        
+Tee ohjelma, joka lukee käyttäjältä lukuja ja tulostaa niiden summan. 
+Ohjelma lopettaa kyselemisen kun syötetään luku 0. 
+Käytä seuraavaa pohjaa, jonka saat myös palautusautomaatilta:
 
-Tee edellistä ohjelmaa vastaava ohjelma, joka laskee kahden kokonaislukumuuttujaan sijoitetun arvon kertolaskun.
+Scanner lukija = new Scanner(System.in);
 
-Esimerkiksi jos muuttujissa on luvut 2 ja 8, ohjelman tulostus on seuraava:
-2 * 8 = 16
-
-Jos taas muuttujissa on luvut 277 ja 111, ohjelman tulostus on seuraava:
-277 * 111 = 30747
-        
-Kokeile myös kuinka suuren kertolaskun ohjelmasi pystyy laskemaan!
-
-        */
-        // Toteuta ohjelmasi tähän -- ota tarvittaessa mallia 
-        // edellisestä tehtävästä. Muista käyttää muuttujia a ja b!
-        int tulos = a * b;
-        System.out.println(a + " * " + b + " = " + tulos);
+int summa = 0;
+System.out.print("Anna lukuja, nolla lopettaa: ");
+while (true) {
+    int luettu = Integer.parseInt(lukija.nextLine());
+    if (luettu == 0) {
+        break;
     }
 
+    // TEE JOTAIN TÄÄLLÄ
+
+}
+System.out.println("Summa lopussa: " + summa);
+
+Ohjelman tulee toimia seuraavasti:
+
+Anna lukuja, nolla lopettaa:
+3
+2
+1
+1
+0
+Summa: 7
+
+
+*/
+import java.util.Scanner;
+
+public class UseammanLuvunSumma {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        int summa = 0;
+
+        System.out.print("Anna lukuja, nolla lopettaa: ");
+        while (true) {
+            int luettu = Integer.parseInt(lukija.nextLine());
+            summa = summa + luettu;
+            if (luettu == 0) {
+                break;
+            }
+
+            // TEE JOTAIN TÄÄLLÄ
+        }
+
+        System.out.println("Summa lopussa: " + summa);
+    }
 }
