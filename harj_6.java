6eac0eaeba772fc10c6eba5f9f27104bc1f03c0b
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-04-03 11:49:51 +0300

    Update harj_6.java

--- harj_6.java
+++ harj_6.java
@@ -1,32 +1,23 @@
-public class Yhteenlasku {
+/*
+Tee ohjelma, joka tulostaa parilliset kokonaisluvut väliltä 2–100.
+
+2
+4
+6
+(välissä paljon rivejä)
+96
+98
+100
+*/
+public class ParillisetLuvut {
 
     public static void main(String[] args) {
-
-        // ÄLÄ KOSKE NÄIHIN
-        /*
-
-
-Tee ohjelma, jonka avulla voidaan laskea kahden kokonaisluvun summa. 
-Ohjelman alussa määritellään kaksi muuttujaa, jotka sisältävät summattavat luvut.
-Voit tarvittaessa käyttää myös muita muuttujia.
-
-Esimerkiksi jos muuttujissa on luvut 5 ja 4, ohjelman tulostus on seuraava:
-5 + 4 = 9
-
-Jos taas muuttujissa on luvut 73457 ja 12888, ohjelman tulostus on seuraava:
-73457 + 12888 = 86345
-
-
-        */
-        int a = 1337;
-        int b = 42;
-
-        int tulos = a + b; // KORJAA TÄMÄ
-
-        // ÄLÄ KOSKE NÄIHIN
-        String tuloste = a + " + " + b + " = " + tulos;
-        System.out.println(tuloste);
-
+        int i = 2;
+        while (i<102){
+            if (i%2==0){
+                System.out.println(i);
+            }
+            i++;
+        }
     }
-
 }
