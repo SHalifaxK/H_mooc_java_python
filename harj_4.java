08bb3aa338b10ae72a72a6cb5a59434be42da33d
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-04-03 11:43:35 +0300

    Update harj_4.java

--- harj_4.java
+++ harj_4.java
@@ -1,42 +1,24 @@
 
-public class MuuttuvatMuuttujat {
+/*
+Tee ohjelma, joka tulostaa kokonaisluvut väliltä 1–100.
+
+Ohjelman tulostus on seuraava:
+
+1
+2
+3
+(välissä paljon rivejä)
+98
+99
+100
+*/
+public class YhdestaSataan {
 
     public static void main(String[] args) {
-        // MUUTA NÄITÄ:
-/*
-        Kanoja:
-3
-Pekonia (kg):
-5.5
-
-Tässä vielä tiivistelmä:
-3
-5.5
-
-Muuta ohjelmaa annetuista kohdista niin että tuloste on:
-
-Kanoja:
-9000
-Pekonia (kg):
-0.1
-
-Tässä vielä tiivistelmä:
-9000
-0.1
-
-        */
-        int kanojenLkm = 9000;
-        double pekoninPaino = 0.1;
-
-        // ÄLÄ MUUTA NÄITÄ:
-        System.out.println("Kanoja:");
-        System.out.println(kanojenLkm);
-        System.out.println("Pekonia (kg):");
-        System.out.println(pekoninPaino);
-        System.out.println("");
-        System.out.println("Tässä vielä tiivistelmä:");
-        System.out.println(kanojenLkm);
-        System.out.println(pekoninPaino);
+        int i =1;
+        while (i<101){
+            System.out.println(i);
+            i++;
+        }
     }
-
 }
