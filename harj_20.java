cfbd97bb539db654d65dae0380c3fccbcbfa7efa
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-04-04 09:53:00 +0300

    Create harj_20.java

---
+++ harj_20.java
@@ -0,0 +1,25 @@
+/*
+Tee metodi tulostaTeksti, joka tulostaa tekstin 
+"Alussa olivat suo, kuokka ja Java." sekä rivinvaihdon.
+
+public static void main(String[] args) {
+    tulostaTeksti();
+}
+
+public static void tulostaTeksti() {
+    // kirjoita koodia tähän
+}
+Ohjelman tulostus:
+Alussa olivat suo, kuokka ja Java.
+
+*/
+public class TekstinTulostus {
+
+    public static void tulostaTeksti() {
+        System.out.println("Alussa olivat suo, kuokka ja Java.");
+    }
+
+    public static void main(String[] args) {
+        tulostaTeksti();
+    }
+}
