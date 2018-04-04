aa51641a399eac99476ff890c9a22bcc7dbe8028
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-04-04 08:49:20 +0300

    Update harj_19.java

--- harj_19.java
+++ harj_19.java
@@ -1,26 +1,109 @@
+/*
+Tee ohjelma, joka kysyy käyttäjältä lukuja (ohjelma tulostaa 
+käyttäjälle aluksi "Syötä luvut:"), kunnes käyttäjä antaa luvun -1. 
+Kun käyttäjä syöttää luvun -1, ohjelma tulostaa 
+"Kiitos ja näkemiin!" ja päättyy.
 
+Syötä luvut:
+5
+2
+4
+-1
+Kiitos ja näkemiin!
+
+Laajenna edellistä ohjelmaa siten, että ohjelma ilmoittaa käyttäjän syöttämien lukujen summan. (Lukua -1 ei lasketa mukaan.)
+
+Syötä luvut:
+5
+2
+4
+-1
+Kiitos ja näkemiin!
+Summa: 11
+
+Tehtävä 19.3: Lukujen summa ja lukumäärä
+
+Laajenna edellistä ohjelmaa siten, että ohjelma ilmoittaa myös käyttäjien antamien lukujen lukumäärän. (Lukua -1 ei lasketa mukaan.)
+
+Syötä luvut:
+5
+2
+4
+-1
+Kiitos ja näkemiin!
+Summa: 11
+Lukuja: 3
+
+Tehtävä 19.4: Lukujen keskiarvo
+
+Muuta edellistä ohjelmaa siten, ohjelma ilmoittaa lukujen keskiarvon. (Lukua -1 ei lasketa mukaan.)
+
+Syötä luvut:
+5
+2
+4
+-1
+Kiitos ja näkemiin!
+Summa: 11
+Lukuja: 3
+Keskiarvo: 3.666666666666
+
+Tehtävä 19.5: Parilliset ja parittomat
+
+Laajenna edellistä ohjelmaa siten, että ohjelma ilmoittaa parillisten ja parittomien lukujen määrän. (Lukua -1 ei lasketa mukaan.)
+
+Syötä luvut:
+5
+2
+4
+-1
+Kiitos ja näkemiin!
+Summa: 11
+Lukuja: 3
+Keskiarvo: 3.666666666666
+Parillisia: 2
+Parittomia: 1
+
+*/
 import java.util.Scanner;
 
-public class PositiivinenLuku {
+public class SilmukatLopetusMuistaminen {
 
     public static void main(String[] args) {
-        Scanner lukija = new Scanner(System.in);
-/*
-Tee ohjelma, joka kysyy käyttäjältä kokonaisluvun ja kertoo, onko se 
-positiivinen (eli suurempi kuin nolla) vai ei.
-Anna luku: 5
-Luku on positiivinen.
+        // Kyseessä on yksi iso tehtävä jota koko ajan laajennetaan -- tehtävä
+        // on viiden yksittäisen tehtäväpisteen arvoinen
 
-Anna luku: -2
-Luku ei ole positiivinen.
-*/
-        // Toteuta ohjelmasi tähän. 
-        System.out.println("Anna luku: ");
-        int luku = Integer.parseInt(lukija.nextLine());
-        if (luku>0){
-            System.out.println("Luku on positiivinen");
-        }else{
-            System.out.println("Luku ei ole positiivinen");
+        // Voit halutessasi lähettää osittain tehdyn tehtäväsarjan tarkastettavaksi palvelimelle
+        // palvelin valittaa tällöin niistä testeistä joita vastaava koodi ei ole vielä
+        // kirjoitettu. Jo tehdyt osat kuitenkin kirjautuvat tehdyiksi.
+        Scanner lukija = new Scanner(System.in);
+        int summa = 0;
+        int count = 0;
+        double keskiarvo = 0;
+        int paril = 0;
+        int parit = 0;
+        while (true){
+            System.out.println("Syötä luvut: ");
+            int luku = Integer.parseInt(lukija.nextLine());
+            if (luku==-1){
+                System.out.println("Kiitos ja näkemiin");
+                System.out.println("Summa: "+ summa);
+                System.out.println("Lukuja: "+ count);
+                keskiarvo = 1.0*summa/count;
+                System.out.println("Keskiarvo: "+keskiarvo);
+                System.out.println("Parillisia:"+paril);
+                System.out.println("Parittomia:"+parit);
+                break;
+            }else{
+                summa = summa + luku;
+                count++;
+                if (luku%2==0){
+                    paril++;
+                }else{
+                    parit++;
+                }
+                continue;
+            }
         }
     }
 }
