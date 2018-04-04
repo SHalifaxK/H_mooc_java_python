5cf5285833974fc00b966b65993770e1d1a227c8
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-04-04 14:53:24 +0300

    Update har1.java

--- har1.java
+++ har1.java
@@ -1,7 +1,32 @@
-public class Nimi {
+/*
+Täydennä tehtäväpohjassa olevaa metodia summa siten, että se laskee 
+ja palauttaa parametrina olevien lukujen summan.
+
+Tee metodi seuraavaan runkoon:
+
+public static int summa(int luku1, int luku2, int luku3, int luku4) {
+    // kirjoita koodia tähän
+    // muista että metodissa on oltava (lopussa) return!
+}
+
+public static void main(String[] args) {
+    int vastaus = summa(4, 3, 6, 1);
+    System.out.println("Summa: " + vastaus);
+}
+
+Ohjelman tulostus:
+
+Summa: 14
+*/
+public class LukujenSumma {
+
+    public static int summa(int luku1, int luku2, int luku3, int luku4) {
+        int summa = luku1+luku2+luku3+luku4;
+        return summa;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Ada Lovelace");
-
+        int vastaus = summa(4, 3, 6, 1);
+        System.out.println("Summa: " + vastaus);
     }
 }
