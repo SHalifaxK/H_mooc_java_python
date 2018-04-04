ca048a623b31269d22699f6169e7b38bdb494e14
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-04-04 14:57:36 +0300

    Update harj2.java

--- harj2.java
+++ harj2.java
@@ -1,7 +1,40 @@
-public class Nimi {
+/*
+Tee metodi pienin, joka palauttaa parametrina saamistaan luvuista 
+pienemmän arvon. 
+
+public static int pienin(int luku1, int luku2) {
+    // kirjoita koodia tähän
+    // älä tulosta metodin sisällä mitään
+
+    // lopussa oltava komento return
+}
+
+public static void main(String[] args) {
+    int vastaus =  pienin(2, 7);
+    System.out.println("Pienin: " + vastaus);
+}
+
+Ohjelman tulostus:
+
+Pienin: 2
+*/
+public class Pienin {
+    public static int pienin(int luku1, int luku2) {
+        int pienin = 0;
+        if (luku1>luku2){
+            pienin = luku2;
+        }else{
+            pienin = luku1;
+        }
+            
+        // älä tulosta metodin sisällä mitään
+        // lopussa oltava komento return
+        return pienin;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hei Maailma!");
-        System.out.println("Ja Mualima!");
+        int vastaus = pienin(2, 7);
+        System.out.println("Pienin: " + vastaus);
     }
 }
+
