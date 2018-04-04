c7f4e4fb53bf4af41f3eac4c7d8b07bfba735fb5
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-04-04 22:01:52 +0300

    Update harj_11.java

--- harj_11.java
+++ harj_11.java
@@ -1,18 +1,12 @@
-
 import java.util.Scanner;
 
-public class SyotetynLuvunTulostaja {
-
+public class Loppuosa {
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-/*
-Tee ohjelma, joka kysyy käyttäjältä lukua ja tulostaa syötetyn luvun arvon.
-Anna luku: 42
-Syötit: 42
-*/
-        // Toteuta ohjelmasi tähän.
-        System.out.println("Anna luku:");
-        int luku = Integer.parseInt(lukija.nextLine());
-        System.out.println("Syötit: "+luku);
+        System.out.println("Anna sana: ");
+        String sana = lukija.nextLine();
+        System.out.println("Loppuosan pituus: ");
+        int pituus = Integer.parseInt(lukija.nextLine());
+        System.out.println("Tulos: "+sana.substring(sana.length()-pituus));
     }
 }
