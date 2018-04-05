af874a2161d92b3e5d6370a1bcf50cb745e0c734
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-04-05 10:27:51 +0300

    Create harj_14.java

---
+++ harj_14.java
@@ -0,0 +1,46 @@
+/*
+Ohjelmaan on toteutettu valmiina pohja, joka lukee käyttäjältä syötteitä 
+listalle. Syötteiden lukeminen päätetään kun käyttäjä syöttää tyhjän 
+merkkijonon.
+
+Ohjelma tulostaa tämän jälkeen listan ensimmäisen arvon. 
+Muokkaa ohjelmaa siten, että ensimmäisen arvon sijaan tulostetaan kolmas arvo. 
+Huomaa, että ohjelmoijat aloittavat laskemisen nollasta! Ohjelma saa rikkoutua 
+rikkoutua jos listalla ei ole vähintään kolmea arvoa. Tarkastele minkälainen 
+virheilmoitus ohjelmasta tällöin tulee.
+
+Terho
+Elina
+Aleksi
+Mari
+
+Aleksi
+
+Elina
+Aleksi
+Mari
+
+Mari
+
+*/
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TietynArvonTulostaminen {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+        ArrayList<String> lista = new ArrayList<>();
+        while (true) {
+            String luettu = lukija.nextLine();
+            if (luettu.equals("")) {
+                break;
+            }
+
+            lista.add(luettu);
+        }
+
+        System.out.println(lista.get(2));
+    }
+}
