f4c7a0f26064b8521f79e9af0c4470824895ddf9
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-04-05 12:59:47 +0300

    Create harj_18.java

---
+++ harj_18.java
@@ -0,0 +1,50 @@
+/*
+Tehtäväpohjassa on ohjelma, joka lukee käyttäjältä syötteitä. 
+Lisää ohjelmaan toiminnallisuus, missä syötteiden lukemisen jälkeen 
+kysytään vielä yhtä merkkijonoa. Ohjelma kertoo tämän jälkeen löytyikö 
+käyttäjän syöttämä merkkijono listalta vai ei.
+
+Terho
+Elina
+Aleksi
+Mari
+
+Ketä etsitään? Mari
+Mari löytyi!
+
+Terho
+Elina
+Aleksi
+Mari
+
+Ketä etsitään? Leevi
+Leevi ei löytynyt!
+
+*/
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LoytyykoListalta {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+        ArrayList<String> lista = new ArrayList<>();
+        while (true) {
+            String luettu = lukija.nextLine();
+            if (luettu.equals("")) {
+                System.out.println("Ketä etsitään? ");
+                String etsitty = lukija.nextLine();
+                if (lista.contains(etsitty)){
+                    System.out.println(etsitty+" löytyi");
+                }else{
+                    System.out.println(etsitty+" ei löytynyt");
+                }
+                break;
+            }
+
+            lista.add(luettu);
+        }
+
+    }
+}
