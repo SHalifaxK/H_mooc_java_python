b2ebf3ac258f0ac8e7cfff98d501a45298baf75d
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-04-05 09:46:15 +0300

    Update harj_12.java

--- harj_12.java
+++ harj_12.java
@@ -1,22 +1,32 @@
+/*
+Tee ohjelma, joka kysyy käyttäjältä kaksi sanaa. Tämän jälkeen ohjelma 
+kertoo onko toinen sana ensimmäisen sanan osana. Käytä ohjelmassa merkkijonon 
+metodia indexOf.
 
+Anna 1. sana: suppilovahvero
+Anna 2. sana: ilo
+Sana 'ilo' on sanan 'suppilovahvero' osana.
+
+Anna 1. sana: suppilovahvero
+Anna 2. sana: suru
+Sana 'suru' ei ole sanan 'suppilovahvero' osana.
+*/
 import java.util.Scanner;
 
-public class KahdenLuvunSummaaja {
+public class SanaSanassa {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-/*
-Tee ohjelma, joka kysyy käyttäjältä kaksi kokonaislukua ja tulostaa niiden summan.
-Anna ensimmäinen luku: 6
-Anna toinen luku: 2
-Lukujen summa: 8
-*/
-        // Toteuta ohjelmasi tähän. Muista kysyä kaksi lukua käyttäjältä!
-        System.out.println("Anna ensimmäinen luku:");
-        int eka = Integer.parseInt(lukija.nextLine());
-        System.out.println("Anna toinen luku:");
-        int toka = Integer.parseInt(lukija.nextLine());
-        int tulos = eka + toka;
-        System.out.println("Lukujen summa: "+ tulos);
+        
+        System.out.println("Anna 1. sana: ");
+        String sana1 = lukija.nextLine();
+        System.out.println("Anna 2. sana: ");
+        String sana2 = lukija.nextLine();
+        int osana = sana1.indexOf(sana2);
+        if (osana==-1){
+            System.out.println("Sana '"+sana2+"' ei ole sanan '"+sana1+"' osana");
+        }else{
+            System.out.println("Sana '"+sana2+"' on sanan '"+sana1+"' osana");
+        }
     }
 }
