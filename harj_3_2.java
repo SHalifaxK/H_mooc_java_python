93398017394be5cb61fe0b37d13cb3ea5bf08562
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-04-17 08:59:25 +0300

    Update and rename harj_3.py to harj_3_2.java

---
+++ harj_3_2.java
@@ -0,0 +1,28 @@
+/*
+Uuden luokan saa lisättyä NetBeansissa seuraavasti: 
+Ruudun vasemmalla reunalla on projektilistaus (Projects). 
+Paina projektin nimen kohdalla hiiren oikeaa nappia. 
+Valitse avautuvasta valikosta New ja Java Class. 
+Anna luokan nimeksi (Class Name) Koira.
+
+Tässä tehtävässä harjoittelet luokan luomista. 
+Luo tehtäväpohjaan luokka nimeltä Koira ja lisää sille oliomuuttujat 
+private String nimi, private String rotu ja private int ika. 
+Luokkakaaviona luokka näyttää seuraavalta:
+
+[Koira|-nimi:String;-rotu:String;-ika:int]
+
+Luokalla ei vielä oikeastaan tee mitään, mutta tämän askeleen harjoittelusta 
+on hyötyä myöhempää ajatellen.
+*/
+import java.util.Scanner;
+
+public class Paaohjelma {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+        // Et pääse juurikaan testailemaan täällä. Seuraa tehtävän ohjeita
+        // täsmällisesti ja aja ohjelman testit.
+    }
+}
