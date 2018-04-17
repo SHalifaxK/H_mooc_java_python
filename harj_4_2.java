1ecf67080943e14286781624b11b791f73d78d1a
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-04-17 09:03:54 +0300

    Create harj_4_2.java

---
+++ harj_4_2.java
@@ -0,0 +1,29 @@
+/*
+Uuden luokan saa lisättyä seuraavasti: Ruudun vasemmalla reunalla 
+on projektilistaus. Paina projektin nimen kohdalla hiiren oikeaa nappia. 
+Valitse avautuvasta valikosta New ja Java Class. Jos haluat että luokan 
+nimi on Luokkahuone, aseta luokan nimeksi (Class Name) Luokkahuone.
+
+Luo luokka nimeltä Luokkahuone. Luokkahuoneella on oliomuuttujina private 
+String koodi, esimerkiksi "B221", ja private int istumapaikat, 
+esimerkiksi 30. Luo tämän jälkeen konstruktori 
+public Luokkahuone(String luokanKoodi, int istumapaikkojenMaara), 
+minkä avulla oliomuuttujiin asetetaan arvot.
+
+[Luokkahuone|-koodi:String;-istumapaikat:int|+Luokkahuone(String‚ int)]
+
+Tälläkään luokalla ei vielä oikeastaan tee mitään, mutta seuraavassa tehtävässä luokastamme tehty olio osaa jo tulostaa tekstiä.
+
+*/
+import java.util.Scanner;
+
+public class Paaohjelma {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+        // Et pääse juurikaan testailemaan täällä. Seuraa tehtävän ohjeita
+        // täsmällisesti ja aja ohjelman testit.
+    }
+
+}
