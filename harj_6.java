a0634dbc6f85a62edb63666d91e073b321961a9f
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-04-17 10:34:30 +0300

    Update harj_6.java

--- harj_6.java
+++ harj_6.java
@@ -1,32 +1,15 @@
-public class Yhteenlasku {
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-    public static void main(String[] args) {
-
-        // ÄLÄ KOSKE NÄIHIN
-        /*
-
-
-Tee ohjelma, jonka avulla voidaan laskea kahden kokonaisluvun summa. 
-Ohjelman alussa määritellään kaksi muuttujaa, jotka sisältävät summattavat luvut.
-Voit tarvittaessa käyttää myös muita muuttujia.
-
-Esimerkiksi jos muuttujissa on luvut 5 ja 4, ohjelman tulostus on seuraava:
-5 + 4 = 9
-
-Jos taas muuttujissa on luvut 73457 ja 12888, ohjelman tulostus on seuraava:
-73457 + 12888 = 86345
-
-
-        */
-        int a = 1337;
-        int b = 42;
-
-        int tulos = a + b; // KORJAA TÄMÄ
-
-        // ÄLÄ KOSKE NÄIHIN
-        String tuloste = a + " + " + b + " = " + tulos;
-        System.out.println(tuloste);
-
+/**
+ *
+ * @author kantomaa
+ */
+public class Ovi {
+    public void koputa(){
+        System.out.println("Who's there?");
     }
-
 }
