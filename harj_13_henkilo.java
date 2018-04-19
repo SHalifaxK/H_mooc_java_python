06a36261819cb9669438165555e6b8fe25576f24
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-04-19 23:38:15 +0300

    Create harj_13_henkilo.java

---
+++ harj_13_henkilo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sakke
+ */
+public class Henkilo {
+    
+    private String nimi;
+    private String numero;
+    
+    public Henkilo(String nimi, String numero){
+        this.nimi = nimi;
+        this.numero = numero;
+    }
+    public String getNimi(){
+        return this.nimi;
+    }
+    public String getNumero(){
+        return this.numero;
+    }
+    public void vaihdaNumeroa(String numero){
+        this.numero = numero;
+    }
+    public String toString(){
+        return this.nimi +" puh: "+this.numero;
+    }
+}
