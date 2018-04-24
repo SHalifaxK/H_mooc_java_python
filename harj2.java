e505865a9249c9bcba0f64e23308cfcd0933742e
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-04-24 12:04:58 +0300

    Update harj2.java

--- harj2.java
+++ harj2.java
@@ -1,7 +1,23 @@
-public class Nimi {
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hei Maailma!");
-        System.out.println("Ja Mualima!");
+        ArrayList<String> rivit = new ArrayList<>();
+
+        // luodaan lukija tiedoston lukemista varten
+        try (Scanner lukija = new Scanner(new File("tiedosto.txt"))) {
+
+        // luetaan kaikki tiedoston rivit
+        while (lukija.hasNextLine()) {
+            rivit.add(lukija.nextLine());
+        }
+        } catch (Exception e) {
+        System.out.println("Virhe: " + e.getMessage());
+        }
+        System.out.println(rivit);
     }
 }
