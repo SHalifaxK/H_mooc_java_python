701070b9b01b37035e7835c195211ce359278fff
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-05-16 10:55:31 +0300

    Create TehtavienhallintaTest.java

---
+++ Tehtavat/src/TehtavienhallintaTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class TehtavienhallintaTest {
+
+    @Test
+    public void tehtavalistaAlussaTyhja() {
+        Tehtavienhallinta hallinta = new Tehtavienhallinta();
+        assertEquals(0, hallinta.tehtavalista().size());
+    }
+
+    @Test
+    public void tehtavanLisaaminenKasvattaaListanKokoaYhdella() {
+        Tehtavienhallinta hallinta = new Tehtavienhallinta();
+        hallinta.lisaa("Kirjoita testi");
+        assertEquals(1, hallinta.tehtavalista().size());
+    }
+
+    @Test
+    public void lisattyTehtavaLoytyyTehtavalistalta() {
+        Tehtavienhallinta hallinta = new Tehtavienhallinta();
+        hallinta.lisaa("Kirjoita testi");
+        assertTrue(hallinta.tehtavalista().contains("Kirjoita testi"));
+    }
+}
